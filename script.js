41a5a45116e7981e6c7476bf6a1389ab3e20d4cc
Alex Chipre <alechipre@gmail.com>
2022-10-18 18:15:04 -0500

    Astivando y desactivando direferentes menus

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const menuEmail = document.querySelector('.navbar-email');
-const menuDesktop = document.querySelector('.desktop-menu')
+const menuDesktop = document.querySelector('.desktop-menu');
+const menuMovile = document.querySelector('.menu');
+const menuHamburger = document.querySelector('.mobile-menu');
+//Variable Carrito de compras
+const carIcon = document.querySelector('.navbar-shopping-cart'); //Icono del carrito
+const asideCar = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', togglemenu);
+menuMovile.addEventListener('click', toggleMenuMovile);
+carIcon.addEventListener('click', toggleAsideCar); //Evento para llamar a la funcion que activa el Aside
 
 function togglemenu(){
   menuDesktop.classList.toggle('inactive');
+
+}
+
+function toggleMenuMovile(){
+  menuHamburger.classList.toggle('inactive');
+}
+
+//funcion para acticvar y desctivar el Aside
+function toggleAsideCar() {
+  asideCar.classList.toggle('inactive');
 }
